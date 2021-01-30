8eb8fa7b862b6476ef91a504bfbe28f82307e423
Cloudymolecule <baires82@gmail.com>
2021-01-30 11:53:41 -0500

    Add helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+    before_action :authorized
+
+    def current_user
+        Guest.find_by(id: session[:guest_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def authorized
+        redirect_to homepage_path unless logged_in?
+    end
 end
