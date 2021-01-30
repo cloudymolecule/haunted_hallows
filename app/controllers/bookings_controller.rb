a9c54d4b5304cbfbb300ffd695b376f2e924a8c6
Cloudymolecule <baires82@gmail.com>
2021-01-30 11:58:50 -0500

    Update guests_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+    before_action :set_booking, only: [:show, :edit, :update, :destroy]
+    
     def index
     end
 
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,4 +1,7 @@
 class GuestsController < ApplicationController
+    before_action :set_guest, only: [:show, :edit, :update, :destroy]
+    skip_before_action :authorized, only: [:new, :create]
+
     def index
     end
 
@@ -6,17 +9,43 @@ class GuestsController < ApplicationController
     end
 
     def new
+        @guest = Guest.new
     end
 
     def edit
     end
 
     def create
+        @guest = Guest.new(guest_params)
+        
+        if @guest.save
+            session[:guest_id] = @guest.id
+            redirect_to @guest
+        else
+            render :new
+        end
     end
 
     def update
     end
 
     def destroy
+    end
+
+    private
+
+    def set_guest
+        @guest = Guest.find(params[:id])
+    end
+
+    def guest_params
+        params.require(:guest).permit(
+            :username, 
+            :email, 
+            :bio,
+            :age, 
+            :gender, 
+            :password, 
+            :password_confirmation)
     end
 end
