a4b4c2dd360ddd43aa03eda620762c6d6867b5eb
Cloudymolecule <baires82@gmail.com>
2021-01-31 13:31:08 -0500

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
     belongs_to :board
 
     validates :entry, presence: true
+
+    def guest_comments
+        @comments = Comment.where(guest_id: current_user.id)
+    end
 end
