fe689033315d34dc63c6a2fee87cb66b2dc0c51d
Cloudymolecule <baires82@gmail.com>
2021-02-04 12:06:45 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,11 @@ Rails.application.routes.draw do
   resources :guests
   resources :rooms
   
-
-  resources :guests, only: [:show, :index, :new] do
-    resources :rooms
-  end
-
-  
-
   resources :boards, only: [:show, :index, :new] do
     resources :comments, shallow: true
   end
   resources :comments
   
-
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
