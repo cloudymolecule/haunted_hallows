c19f70c9a1b1808363f21a3549a83b12c82d7689
Cloudymolecule <baires82@gmail.com>
2021-02-04 09:50:51 -0500

    Add dnd method in rooms_helper.rb

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,11 @@
 module RoomsHelper
+
+    def dnd(room)
+        if room.do_not_disturb == true
+            "Do Not Disturb, this room is private"
+        else
+            "This room is public."
+        end
+    end
+
 end
